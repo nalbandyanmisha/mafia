c38887cbe5adc22e73ed58e22efbe9d67da91d44
nalbandyanmisha <nalbandyanmisha@gmail.com>
2025-11-27 20:03:16 +0400

    Add main commands as interface

--- src/commands.rs
+++ src/commands.rs
@@ -1,32 +1,80 @@
 use clap::{Parser, Subcommand};
+use std::io::{self, Write};
 
 #[derive(Debug, Subcommand)]
 pub enum Command {
+    Registration(Args),
     Start(StartArgs),
+    New(Players),
+    End,
+    Quite,
+}
+
+#[derive(Debug, Parser)]
+struct Args {
+    pub name: String,
 }
 
 impl Command {
     pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
         match self {
             Command::Start(cmd) => cmd.run().await,
+            Command::End => {
+                println!("Ending the current game");
+                // Here you would add the logic to end the game
+                Ok(())
+            }
+            Command::New(players) => {
+                println!("Creating a new game with players: {:?}", players.names);
+                // Here you would add the logic to create a new game
+                Ok(())
+            }
+            Command::Quite => {
+                println!("Ending the current game");
+                // Here you would add the logic to end the game
+                Ok(())
+            }
+            Command::Registration(args) => {
+                println!("Registering player: {}", args.name);
+                Ok(())
+            }
         }
     }
 }
 
 #[derive(Debug, Parser)]
 pub struct StartArgs {
-    #[clap(
-        long,
-        default_value = "7",
-        help = "Number of signers to derive from mnemonic"
-    )]
+    #[clap(long, default_value = "10", help = "Number of players to start game")]
     pub players: Option<usize>,
 }
 
 impl StartArgs {
     pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
         println!("Starting a new game with {} players", self.players.unwrap());
+
         // Here you would add the logic to start the game
+        Ok(())
+    }
+}
+
+#[derive(Debug, Parser)]
+struct Players {
+    pub names: Vec<String>,
+}
+
+impl Players {
+    pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
+        println!("Creating a new game with players: {:?}", self.names.len());
+        println!("Enter player names separated by spaces:");
+        print!("> ");
+        io::stdout().flush()?;
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        let names = input.trim().split_whitespace();
+        let mut clap_args = vec!["mafia"];
+        clap_args.extend(names);
+
+        // Here you would add the logic to create a new game
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,60 @@
 mod commands;
 use clap::Parser;
 use commands::Command;
+use std::io::{self, Write};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mafia = Mafia::parse();
+
+    println!("Starting Mafia CLI");
 
     mafia.run().await?;
     Ok(())
 }
 
 #[derive(Parser, Debug)]
-#[command(name = "mafia", version, about = "Host of Mafia game", long_about = None)]
+#[command(name = "mafia", version, about = "The host of Mafia game", long_about = None)]
 struct Mafia {
     #[command(subcommand)]
-    command: Option<Command>,
+    pub command: Option<Command>,
 }
 
 impl Mafia {
     pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
-        if let Some(command) = &self.command {
-            command.run().await?;
-        } else {
-            println!("Welcome to Mafia, type the command to move forward");
+        loop {
+            print!("> ");
+            io::stdout().flush()?;
+            let mut input = String::new();
+            io::stdin().read_line(&mut input)?;
+            let args = input.trim().split_whitespace();
+            let mut clap_args = vec!["mafia"];
+            clap_args.extend(args);
+            let mafia = Mafia::parse_from(&clap_args);
+            match &mafia.command {
+                Some(Command::Quite) => {
+                    println!("Quitting the Mafia CLI. Goodbye!");
+                    break;
+                }
+                Some(Command::Start(cmd)) => {
+                    cmd.run().await?;
+                }
+                cmd => {
+                    println!("Running command: {cmd:#?}");
+                    println!("----------------------------------------");
+                    println!("Type the command and press Enter to execute it.");
+                }
+            }
+
+            // if let Some(command) = &self.command {
+            //     if let Command::Quite = command {
+            //         println!("Quitting the Mafia CLI. Goodbye!");
+            //         break;
+            //     }
+            //     println!("Running command: {command:#?}");
+            //     println!("----------------------------------------");
+            //     println!("Type the command and press Enter to execute it.");
+            // }
         }
         Ok(())
     }
