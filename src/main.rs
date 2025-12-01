553c4500cdcf825e46018049d149c9d9d0460656
nalbandyanmisha <nalbandyanmisha@gmail.com>
2025-12-01 21:26:08 +0400

    Add game phases and voting system

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{Parser, Subcommand};
 use rand::prelude::*;
+use std::collections::HashMap;
 use std::io::Write;
 
 #[derive(Debug, Default, Clone)]
@@ -8,6 +9,16 @@ struct Player {
     pub name: String,
     pub role: Role,
     pub warnings: u8,
+    pub status: Status,
+    pub is_nominated: bool,
+}
+
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+enum Status {
+    #[default]
+    Alive,
+    Dead,
+    Left,
 }
 
 impl Player {
@@ -17,11 +28,31 @@ impl Player {
             name,
             role,
             warnings: 0,
+            status: Status::Alive,
+            is_nominated: false,
         }
     }
 
     fn add_warning(&mut self) {
         self.warnings += 1;
+    }
+
+    fn reset_warnings(&mut self) {
+        self.warnings = 0;
+    }
+
+    fn remove_warning(&mut self) {
+        if self.warnings > 0 {
+            self.warnings -= 1;
+        }
+    }
+
+    fn withdraw(&mut self) {
+        self.is_nominated = false;
+    }
+
+    fn nominate(&mut self) {
+        self.is_nominated = true;
     }
 }
 
@@ -32,12 +63,6 @@ enum Role {
     Mafia,
     Don,
     Sheriff,
-}
-
-struct Table {
-    chairs: Vec<Chair>,
-    available_roles: Vec<Role>,
-    available_positions: Vec<u8>,
 }
 
 #[derive(Debug, Default, Clone)]
@@ -57,8 +82,14 @@ impl Chair {
     }
 }
 
-struct Voting {
-    pub players: Vec<Player>,
+struct Table {
+    chairs: Vec<Chair>,
+    available_roles: Vec<Role>,
+    available_positions: Vec<u8>,
+    nominated_players: Vec<u8>,
+    votes: HashMap<u8, Vec<u8>>, // (nominee, votes)
+    phase: Phase,
+    round: u8,
 }
 
 impl Table {
@@ -82,6 +113,10 @@ impl Table {
             chairs,
             available_roles,
             available_positions,
+            nominated_players: Vec::new(),
+            votes: HashMap::new(),
+            phase: Phase::default(),
+            round: 0,
         }
     }
 
@@ -109,6 +144,15 @@ impl Table {
     }
 }
 
+#[derive(Parser, Debug, Default)]
+enum Phase {
+    #[default]
+    Night,
+    Morning,
+    Day,
+    Voting,
+}
+
 #[derive(Parser, Debug)]
 #[command(name = "mafia", version, about = "Mafia game host CLI")]
 struct MafiaCli {
@@ -121,6 +165,13 @@ enum Action {
     Join { name: String },
     Left { position: u8 },
     Warn { position: u8 },
+    Pardon { position: u8 },
+    Nominate { position: u8 },
+    Withdraw { position: u8 },
+    Shoot { position: u8 },
+    Voting,
+    Day,
+    Night,
     Show,
     Print,
     Quite,
@@ -201,6 +252,99 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 } else {
                     println!("Invalid seat number: {pos}");
                 }
+            }
+            Some(Action::Pardon { position }) => {
+                let pos = *position;
+                if let Some(chair) = table.chairs.get_mut(usize::from(pos - 1)) {
+                    if let Some(player) = &mut chair.player {
+                        player.remove_warning();
+                        println!(
+                            "Player {} at seat {} has been pardoned. Total warnings: {}",
+                            player.name, pos, player.warnings
+                        );
+                    } else {
+                        println!("No player at seat {pos} to pardon.");
+                    }
+                } else {
+                    println!("Invalid seat number: {pos}");
+                }
+            }
+            Some(Action::Shoot { position }) => {
+                let pos = *position;
+                if let Some(chair) = table.chairs.get_mut(usize::from(pos - 1)) {
+                    if let Some(player) = &mut chair.player {
+                        player.status = Status::Dead;
+                        println!("Player {} at seat {} has been shot.", player.name, pos);
+                    } else {
+                        println!("No player at seat {pos} to shoot.");
+                    }
+                } else {
+                    println!("Invalid seat number: {pos}");
+                }
+            }
+            Some(Action::Nominate { position }) => {
+                let pos = *position;
+                if let Some(chair) = table.chairs.get_mut(usize::from(pos - 1)) {
+                    if let Some(player) = &mut chair.player {
+                        player.nominate();
+                        println!("Player {} at seat {} has been nominated.", player.name, pos);
+                    } else {
+                        println!("No player at seat {pos} to nominate.");
+                    }
+                } else {
+                    println!("Invalid seat number: {pos}");
+                }
+                table.nominated_players.push(pos);
+            }
+            Some(Action::Withdraw { position }) => {
+                let pos = *position;
+                if let Some(chair) = table.chairs.get_mut(usize::from(pos - 1)) {
+                    if let Some(player) = &mut chair.player {
+                        player.withdraw();
+                        println!(
+                            "Player {} at seat {} has withdrawn nomination.",
+                            player.name, pos
+                        );
+                    } else {
+                        println!("No player at seat {pos} to withdraw.");
+                    }
+                } else {
+                    println!("Invalid seat number: {pos}");
+                }
+                if let Some(index) = table.nominated_players.iter().position(|x| *x == *position) {
+                    table.nominated_players.remove(index);
+                }
+            }
+            Some(Action::Voting) => {
+                table.phase = Phase::Voting;
+                println!("It is now Voting phase.");
+                println!("Nominated players in order: {:?}", table.nominated_players);
+                for nominee in &table.nominated_players {
+                    println!("Collecting votes for seat {nominee}.");
+                    let mut input = String::new();
+                    std::io::stdin().read_line(&mut input)?;
+                    let votes = input
+                        .split(',')
+                        .filter_map(|s| s.trim().parse::<u8>().ok())
+                        .collect();
+
+                    table.votes.insert(*nominee, votes);
+                }
+            }
+            Some(Action::Day) => {
+                table.phase = Phase::Day;
+                println!("It is now Day {}.", table.round);
+            }
+            Some(Action::Night) => {
+                table.phase = Phase::Night;
+                table.round += 1;
+                table.nominated_players = Vec::new();
+                for player in &mut table.chairs {
+                    if let Some(p) = &mut player.player {
+                        p.withdraw();
+                    }
+                }
+                println!("It is now Night {}.", table.round);
             }
             Some(Action::Print) => {
                 for chair in &table.chairs {
